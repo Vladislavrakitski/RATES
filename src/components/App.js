dd793b002ed77771e15afd376fb05cae05181c30
Vladislav Rakitski <38129489+Vladislavrakitski@users.noreply.github.com>
2020-01-21 19:47:44 +0300

    Update App.js
    remove styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Card from './card.js'
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/app.css';
 
 export default class App extends Component{
   constructor(props){
